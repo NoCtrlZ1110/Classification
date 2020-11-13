14bc2de96c89b6eeab3c4de28a49cb23d79f7413
Huy <huynd.nguyen@gmail.com>
2020-11-13 13:17:18 +0700

    finish question 5

---
+++ README.md
@@ -0,0 +1,35 @@
+# <b>Nguyễn Văn Huy - 18020651</b>
+
+## Question 1 - Perceptron
+
+```python
+python dataClassifier.py -c perceptron 
+```
+
+## Question 2 - Perceptron Analysis
+
+```python
+python dataClassifier.py -c perceptron -w 
+```
+
+## Question 3 - MIRA
+```python
+python dataClassifier.py -c mira --autotune 
+```
+
+## Question 4 - Digit Feature Design
+```python
+python dataClassifier.py -d digits -c naiveBayes -f -a -t 1000  
+```
+
+## Question 5 - Behavioral Cloning
+```python
+python dataClassifier.py -c perceptron -d pacman
+```
+
+## Question 6 - Pacman Feature Design
+```python
+python dataClassifier.py -c perceptron -d pacman -f -g ContestAgent -t 1000 -s 1000
+```
+
+
--- perceptron_pacman.py
+++ perceptron_pacman.py
@@ -4,7 +4,7 @@
 # educational purposes provided that (1) you do not distribute or publish
 # solutions, (2) you retain this notice, and (3) you provide clear
 # attribution to UC Berkeley, including a link to http://ai.berkeley.edu.
-# 
+#
 # Attribution Information: The Pacman AI projects were developed at UC Berkeley.
 # The core projects and autograders were primarily created by John DeNero
 # (denero@cs.berkeley.edu) and Dan Klein (klein@cs.berkeley.edu).
@@ -25,10 +25,10 @@ class PerceptronClassifierPacman(PerceptronClassifier):
         PerceptronClassifier.__init__(self, legalLabels, maxIterations)
         self.weights = util.Counter()
 
-    def classify(self, data ):
+    def classify(self, data):
         """
         Data contains a list of (datum, legal moves)
-        
+
         Datum is a Counter representing the features of each GameState.
         legalMoves is a list of legal moves for that GameState.
         """
@@ -36,13 +36,14 @@ class PerceptronClassifierPacman(PerceptronClassifier):
         for datum, legalMoves in data:
             vectors = util.Counter()
             for l in legalMoves:
-                vectors[l] = self.weights * datum[l] #changed from datum to datum[l]
+                # changed from datum to datum[l]
+                vectors[l] = self.weights * datum[l]
             guesses.append(vectors.argMax())
         return guesses
 
-
-    def train( self, trainingData, trainingLabels, validationData, validationLabels ):
-        self.features = trainingData[0][0]['Stop'].keys() # could be useful later
+    def train(self, trainingData, trainingLabels, validationData, validationLabels):
+        # could be useful later
+        self.features = trainingData[0][0]['Stop'].keys()
         # DO NOT ZERO OUT YOUR WEIGHTS BEFORE STARTING TRAINING, OR
         # THE AUTOGRADER WILL LIKELY DEDUCT POINTS.
 
@@ -50,4 +51,22 @@ class PerceptronClassifierPacman(PerceptronClassifier):
             print "Starting iteration ", iteration, "..."
             for i in range(len(trainingData)):
                 "*** YOUR CODE HERE ***"
-                util.raiseNotDefined()
+                scores = util.Counter()
+
+                for a in trainingData[i][1]:
+                    # xac dinh score
+                    score = util.Counter()
+                    score = trainingData[i][0][a] * self.weights
+                    scores[a] = score
+
+                # lay ra action nhieu diem nhat
+
+                guess = scores.argMax()
+
+                # if our estimate is incorrect, adjust the weights accordingly
+                if(trainingLabels[i] != guess):
+                    # Based on the correct action, increase the weight
+                    self.weights = self.weights + \
+                        trainingData[i][0][trainingLabels[i]]
+                    # Based on the geussed action, decrease the weight
+                    self.weights = self.weights - trainingData[i][0][guess]
